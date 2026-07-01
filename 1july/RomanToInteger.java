f25f15d8019926239eea5215c334b18d3489823d
Bhumi Sachdeva <bhumi.sachdeva25@gmail.com>
2026-07-01 12:01:51 +0530

    Added daily progress

---
+++ 1july/RomanToInteger.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class RomanToInteger {
+
+    public static int romanToInt(String s) {
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = value(s.charAt(i));
+
+            if (i < s.length() - 1) {
+                int next = value(s.charAt(i + 1));
+
+                if (current < next) {
+                    total = total - current;
+                } else {
+                    total = total + current;
+                }
+            } else {
+                total = total + current;
+            }
+        }
+
+        return total;
+    }
+
+    public static int value(char ch) {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        String roman = sc.nextLine().toUpperCase();
+
+        int result = romanToInt(roman);
+
+        System.out.println("Integer value = " + result);
+
+        sc.close();
+    }
+}
